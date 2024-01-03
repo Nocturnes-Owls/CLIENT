277efe1cbd9f200f10de7abce775bdef66c2c928
julian.castaño_a <juliancaristizabal@gmail.com>
2024-01-03 01:19:40 -0500

    navbar blur effect

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const NavBar = () => {
+  const navBar = document.querySelector(".navbar-container");
+
+  function blurAnimation() {
+    if ( window.scrollY > 100 ){
+      navBar.classList.add("navbar-container-blur")
+    } else {
+      navBar.classList.remove("navbar-container-blur");
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", blurAnimation);
+  }, []);
+
   return (
     <div className="navbar-container">
       <nav>
         <span>
-          <h2>
-            LOGO
-          </h2>
+          <h2>LOGO</h2>
         </span>
-        <menu>
-          III
-        </menu>
+        <menu>III</menu>
       </nav>
     </div>
   );
