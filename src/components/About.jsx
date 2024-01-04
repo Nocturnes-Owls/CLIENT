8717ce93a50a7580563d4406e0a58ba2d2b89352
julian.castaño_a <juliancaristizabal@gmail.com>
2024-01-04 01:23:27 -0500

    add About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,36 +2,40 @@ import React from "react";
 
 const About = ({ title, text }) => {
   return (
-    <div className="about-container">
-      <section className="about-grid">
-        {/* <h1>{title}</h1>
-        <p>{text}</p>
-        <p></p> */}
-        <div>
-          <h1>ABOUT US</h1>
-          <h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
-            asperiores aut nam aliquid molestiae deserunt culpa, pariatur quis
-            numquam odit nulla, ullam similique at amet ea voluptas eius
-            quisquam in.
-          </h2>
+    <section className="section about-container">
+      {/* Seccion texto e imagenes */}
+      <div className="about-content container">
+        {/* texto */}
+        <div className="about-textside">
+          <h1 className="title">ABOUT US</h1>
+
+          <div>
+            <p className="subtitle">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              asperiores aut nam aliquid molestiae deserunt culpa, pariatur quis
+              numquam odit nulla, ullam similique at amet ea voluptas eius
+              quisquam in.
+            </p>
+            <p className="subtitle">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              asperiores aut nam aliquid molestiae deserunt culpa, pariatur quis
+              numquam odit nulla, ullam similique at amet ea voluptas eius
+              quisquam in.
+            </p>
+          </div>
+
+          <button className="subtitle">CONTACT US</button>
         </div>
 
-        <div>
-          <h1>Imagen</h1>
+        <div className="about-media">
+          <div></div>
+          <div></div>
+          <div className="img-container">
+            <img src="buho-about.webp" alt="" />
+          </div>
         </div>
-
-        <div>
-          <h1>Imagen</h1>
-        </div>
-
-        <div>
-          <button>
-            CONTACT US
-          </button>
-        </div>        
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
