aa73d5f0ac9d7cb69513c5eb42f6721117402d63
Keny Espinoza Luis <kespinozaluis@gmail.com>
2024-01-09 10:50:03 -0500

    update video hackaton

--- src/components/Tournament.jsx
+++ src/components/Tournament.jsx
@@ -4,7 +4,7 @@ const Tournament = ({title}) => {
     <section id="tournament" className='section tournament'>
       <div className='container'>
         <div className="hackaton-video">
-          <video src="/Hackaton-NocturnesOwls.mp4" type="video/mp4" controls={true} poster="hackatonPoster.webp"></video>
+          <video src="/hackaton.mp4" type="video/mp4" controls={true} poster="hackatonPoster.webp"></video>
         </div>
         <div className="hackaton-description">
           <div className="hackaton-title">
@@ -12,7 +12,7 @@ const Tournament = ({title}) => {
           </div>
           <div className="hackaton-btn">
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSeuWY9QghSuQvcTpgVvXR-AGreLnqLES5Qu-4NM_yiVOJsZgw/viewform" className="btn btnPrimary" target="_blank" rel="noopener noreferrer"> <span className="btn-text">Registro</span></a>
-            <a href="https://drive.google.com/file/d/1pMS7smX9yma_0i_UQql0671A3sSUW86e/view?usp=drive_link" download="Torneo-Nocturne-Owl" className="btn btnPrimary" target="_blank" rel="noopener noreferrer"><span className="btn-text">Más Información</span></a>
+            <a href="https://drive.google.com/file/d/1WBfVNIARlMxOWQEndrBSHI6uqRjYyuYg/view?usp=drive_link" download="Torneo-Nocturne-Owl" className="btn btnPrimary" target="_blank" rel="noopener noreferrer"><span className="btn-text">Más Información</span></a>
           </div>
           <div className="hackaton-sponsor">
             <a href="#"><img src="/napoleon-chat.png" alt="Napoleon chat" /></a>
