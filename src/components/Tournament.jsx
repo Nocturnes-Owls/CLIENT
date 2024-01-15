2fc4ad35a0467f3180fc3208cb0420538b5d25a3
Jorge Bastidas <jorgebastidas9@gmail.com>
2024-01-15 10:01:44 -0400

    change link and comment register

--- src/components/Tournament.jsx
+++ src/components/Tournament.jsx
@@ -18,8 +18,8 @@ const Tournament = ({title}) => {
             <h1 className="title">{title}</h1>
           </div>
           <div className="hackaton-btn">
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSeuWY9QghSuQvcTpgVvXR-AGreLnqLES5Qu-4NM_yiVOJsZgw/viewform" className="btn btnPrimary" target="_blank" rel="noopener noreferrer"> <span className="btn-text">Registro</span></a>
-            <a href="https://drive.google.com/file/d/1WBfVNIARlMxOWQEndrBSHI6uqRjYyuYg/view?usp=drive_link" download="Torneo-Nocturne-Owl" className="btn btnPrimary" target="_blank" rel="noopener noreferrer"><span className="btn-text">Información</span></a>
+            {/* <a href="https://docs.google.com/forms/d/e/1FAIpQLSeuWY9QghSuQvcTpgVvXR-AGreLnqLES5Qu-4NM_yiVOJsZgw/viewform" className="btn btnPrimary" target="_blank" rel="noopener noreferrer"> <span className="btn-text">Registro</span></a> */}
+            <a href="https://drive.google.com/file/d/1Pv-H23PLGxppZlXqaYwJZA4V_FSBOyhY/view?usp=drivesdk" download="Torneo-Nocturne-Owl" className="btn btnPrimary" target="_blank" rel="noopener noreferrer"><span className="btn-text">Información</span></a>
           </div>
           <div className="hackaton-sponsor">
             <a href="https://napoleon.chat/" target="_blank"><img src="/sponsors/napoleon-chat.png" alt="Napoleon chat" /></a>
